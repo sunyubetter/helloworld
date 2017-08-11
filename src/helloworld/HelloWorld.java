baa722b5a8e6ca18d77e9c2d5c18514f086e95e5
sun <249640465@qq.com>
2017-08-11 16:34:57 +0800

    first commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,24 @@
+/*
+ * 
+ * 
+ * 
+ */
+package helloworld;
+
+/**
+ *
+ * @author Yu Sun
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("HELLO WORLD!!");
+        }
+    }
+
+}
